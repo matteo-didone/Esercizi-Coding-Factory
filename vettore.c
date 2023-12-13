f440c4a30f028d54e14948c20da82e52f59b6a0a
Matteo Didone <matteo.dido9@gmail.com>
2023-12-13 17:18:56 +0100

    Done with exercise on vector.c and matrix.c

--- vettore.c
+++ vettore.c
@@ -3,60 +3,117 @@
 // Scopo del programma:
 // trova il valore minimo all'interno di tre vettori di interi
 // di dimensione qualsiasi
+
+
 // LIBRERIE DA INCLUDERE
 #include <stdio.h>
+
 // VARIABILI GLOBALI
 // PROTOTIPI
-int cercaMinimo(int v[],int n);
-void visualizza(int v[],int n);
+int cercaMinimo(int v[], int n);
+int cercaMassimo(int v[], int n);
+int calcolaMedia(int v[], int n);
+void visualizza(int v[], int n);
+
 // PROGRAMMA PRINCIPALE
-int main(){
-// VARIABLI LOCALI AL MAIN
- int v1[]={4,3,5,9,7,1};
- int v2[]={5,7,8,6,2,4,9};
- int v3[]={-3,5,4,0};
- int dimv1,dimv2,dimv3,minv1,minv2,minv3;
-// CORPO DEL PROGRAMMA
- printf("inizio\n"); 
- dimv1=sizeof(v1)/sizeof(v1[0]);
- dimv2=sizeof(v2)/sizeof(v2[0]);
- dimv3=sizeof(v3)/sizeof(v3[0]);
- visualizza(v1,dimv1);
- minv1=cercaMinimo(v1,dimv1);
- printf("valore minimo: %d\n",minv1);
- visualizza(v2,dimv2);
- minv2=cercaMinimo(v2,dimv2);
- printf("valore minimo: %d\n",minv2);
- visualizza(v3,dimv3);
- minv3=cercaMinimo(v3,dimv3);
- printf("valore minimo: %d\n",minv3);
- printf("\nfine\n");
-// USCITA DAL PROGRAMMA - codice di uscita dal programma =0
- return 0;
+int main()
+{
+	// VARIABLI LOCALI AL MAIN
+	int v1[] = {4, 3, 5, 9, 7, 1};
+	int v2[] = {5, 7, 8, 6, 2, 4, 9};
+	int v3[] = {-3, 5, 4, 0};
+	int dimv1, dimv2, dimv3, minv1, minv2, minv3, maxv1, maxv2, maxv3, avgv1, avgv2, avgv3;
+
+	// CORPO DEL PROGRAMMA
+	printf("inizio\n");
+
+	// Andiamo a prendere la dimenisone dei byte degli array, e la dividiamo per la dimensione in byte del singolo elemento!
+	dimv1 = sizeof(v1) / sizeof(v1[0]);
+	dimv2 = sizeof(v2) / sizeof(v2[0]);
+	dimv3 = sizeof(v3) / sizeof(v3[0]);
+
+	visualizza(v1, dimv1);
+	minv1 = cercaMinimo(v1, dimv1);
+	maxv1 = cercaMassimo(v1, dimv1);
+	avgv1 = calcolaMedia(v1, dimv1);
+	printf("valore minimo: %d\n", minv1);
+	printf("valore massimo: %d\n", maxv1);
+	printf("valore medio: %d\n", avgv1);
+
+
+	visualizza(v2, dimv2);
+	minv2 = cercaMinimo(v2, dimv2);
+	maxv2 = cercaMassimo(v2, dimv2);
+	avgv2 = calcolaMedia(v2, dimv2);
+	printf("valore minimo: %d\n", minv2);
+	printf("valore massimo: %d\n", maxv2);
+	printf("valore medio: %d\n", avgv2);
+
+	visualizza(v3, dimv3);
+	minv3 = cercaMinimo(v3, dimv3);
+	maxv3 = cercaMassimo(v3, dimv3);
+	avgv3 = calcolaMedia(v3, dimv3);
+	printf("valore minimo: %d\n", minv3);
+	printf("valore massimo: %d\n", maxv3);
+	printf("valore medio: %d\n", avgv3);
+	
+
+	printf("\nfine\n");
+
+	// USCITA DAL PROGRAMMA - codice di uscita dal programma =0
+	return 0;
 }
 // FUNZIONI
 
-//trova il valore minimo
-int cercaMinimo(int v[],int n)
-{ 
-// VARIABILI LOCALI ALLA FUNZIONE
+// trova il valore minimo
+int cercaMinimo(int v[], int n)
+{
+	// VARIABILI LOCALI ALLA FUNZIONE
 	int i, min;
-//CORPO DELLA FUNZIONE
-	min=v[0];
-	for (i=1; i<n; i++)
-		if (v[i]<min)
-			min=v[i];
+	// CORPO DELLA FUNZIONE
+	min = v[0];
+	for (i = 1; i < n; i++)
+		if (v[i] < min)
+			min = v[i];
 	return min;
 }
 
-//visualizza in riga gli elementi di un vettore
-void visualizza(int v[],int n)
-{// VARIABILI LOCALI ALLA FUNZIONE
+// trova il valore massimo
+int cercaMassimo(int v[], int n)
+{
+	// VARIABILI LOCALI ALLA FUNZIONE
+	int i, max;
+	// CORPO DELLA FUNZIONE
+	max = v[0];
+	for (i = 1; i < n; i++)
+		if (v[i] > max) 
+			max = v[i];
+	return max;
+}
+//calcola la media aritmetica dei valori contenuti nell'array
+int calcolaMedia(int v[], int n) 
+{
+	//VARIABILI LOCALI ALLA FUNZIONE
+	int i, sumOfElements, avg;
+
+	//CORPO DELLA FUNZIONE
+	for (i = 0; i < n; i++) 
+	{
+		sumOfElements += v[i];
+	}
+	avg = sumOfElements / n;
+
+	return avg; 
+}
+
+// visualizza in riga gli elementi di un vettore
+void visualizza(int v[], int n)
+{ // VARIABILI LOCALI ALLA FUNZIONE
 	int i;
-//CORPO DELLA FUNZIONE
+	// CORPO DELLA FUNZIONE
 	printf("\nElementi: ");
-	for (i=0; i<n; i++)
-		printf("%d ",v[i]);
+	for (i = 0; i < n; i++)
+		printf("%d ", v[i]);
 	printf("\n");
 	return;
 }
